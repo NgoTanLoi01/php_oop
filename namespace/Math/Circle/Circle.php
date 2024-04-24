83ac0e1e620966639527327f803ae3a0eba24c8d
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-04-24 10:27:25 +0700

    refactor: namspace php part3

--- namespace/Math/Circle/Circle.php
+++ namespace/Math/Circle/Circle.php
@@ -3,8 +3,17 @@
 namespace Math\Circle;
 
 use Math\Constant;
+use DateTime;
 
 require_once __DIR__ . '/../Constant.php';
+
+function strlen($str)
+{
+    echo $str;
+    return \strlen($str) - 1;
+}
+
+
 
 
 class Circle
@@ -12,5 +21,11 @@ class Circle
     public function getAreaCicle($radius)
     {
         return Constant::PI * $radius * $radius;
+    }
+
+    public function getTimeStamp()
+    {
+        $dateTime = new DateTime();
+        return $dateTime->getTimestamp();
     }
 }
--- namespace/index.php
+++ namespace/index.php
@@ -2,4 +2,6 @@
 require('Math/Circle/Circle.php');
 
 $circle = new Math\Circle\Circle();
-echo $circle->getAreaCicle(10);
+//echo $circle->getAreaCicle(10);
+//echo $circle->getTimeStamp();
+echo strlen('loi');
